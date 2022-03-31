a7ad5f47cc0168a18f382d34060dea974c700172
TobiasTac <95323145+TobiasTac@users.noreply.github.com>
2022-03-31 11:21:58 -0300

    commit
    commit change

---
+++ script.js
@@ -0,0 +1,158 @@
+function idosoJs() {
+    let idosof1 = 0;        
+
+    switch (idosof1){
+        case 0: {
+    document.querySelector("#result1").style.display = "block";
+    document.getElementById("result1").innerHTML = "Fique em casa, peça para fazerem compras por você!";
+    document.querySelector("#result2").style.display = "none";
+    document.querySelector("#result3").style.display = "none";
+    document.querySelector("#result4").style.display = "none";
+    }
+    break;
+    case 1: {
+        document.querySelector("#result2").style.display = "block";
+        document.getElementById("result2").innerHTML = "Fique em casa, tome Sol e faça exercícios leves!";
+        document.querySelector("#result1").style.display = "none";
+        document.querySelector("#result3").style.display = "none";
+        document.querySelector("#result4").style.display = "none";
+    }
+    break;
+    case 2: {
+        document.querySelector("#result3").style.display = "block";
+        document.getElementById("result3").innerHTML = "Fique em casa e cuide da sua saúde!";
+        document.querySelector("#result1").style.display = "none";
+        document.querySelector("#result2").style.display = "none";
+        document.querySelector("#result4").style.display = "none";
+    }
+    break;
+    case 3: {
+        document.querySelector("#result4").style.display = "block";
+        document.getElementById("result4").innerHTML = "Faça compras pelos mais idosos e use máscara!";
+        document.querySelector("#result1").style.display = "none";
+        document.querySelector("#result2").style.display = "none";
+        document.querySelector("#result3").style.display = "none";
+    }
+    break;
+    default: console.log("Algo de errado não está certo");
+}
+}
+
+function respJs(){
+    let respif2 = 1;
+    switch (respif2){
+    case 0: {
+        document.querySelector("#result1").style.display = "block";
+        document.getElementById("result1").innerHTML = "Fique em casa, peça para fazerem compras por você!";
+        document.querySelector("#result2").style.display = "none";
+        document.querySelector("#result3").style.display = "none";
+        document.querySelector("#result4").style.display = "none";
+        }
+        break;
+        case 1: {
+            document.querySelector("#result2").style.display = "block";
+            document.getElementById("result2").innerHTML = "Fique em casa, tome Sol e faça exercícios leves!";
+            document.querySelector("#result1").style.display = "none";
+            document.querySelector("#result3").style.display = "none";
+            document.querySelector("#result4").style.display = "none";
+        }
+        break;
+        case 2: {
+            document.querySelector("#result3").style.display = "block";
+            document.getElementById("result3").innerHTML = "Fique em casa e cuide da sua saúde!";
+            document.querySelector("#result1").style.display = "none";
+            document.querySelector("#result2").style.display = "none";
+            document.querySelector("#result4").style.display = "none";
+        }
+        break;
+        case 3: {
+            document.querySelector("#result4").style.display = "block";
+            document.getElementById("result4").innerHTML = "Faça compras pelos mais idosos e use máscara!";
+            document.querySelector("#result1").style.display = "none";
+            document.querySelector("#result2").style.display = "none";
+            document.querySelector("#result3").style.display = "none";
+        }
+        break;
+        default: console.log("Algo de errado não está certo");
+}
+
+}
+function diabeteJs(){
+    let diabetef3 = 2;
+    switch (diabetef3){
+    case 0: {
+        document.querySelector("#result1").style.display = "block";
+        document.getElementById("result1").innerHTML = "Fique em casa, peça para fazerem compras por você!";
+        document.querySelector("#result2").style.display = "none";
+        document.querySelector("#result3").style.display = "none";
+        document.querySelector("#result4").style.display = "none";
+        }
+        break;
+        case 1: {
+            document.querySelector("#result2").style.display = "block";
+            document.getElementById("result2").innerHTML = "Fique em casa, tome Sol e faça exercícios leves!";
+            document.querySelector("#result1").style.display = "none";
+            document.querySelector("#result3").style.display = "none";
+            document.querySelector("#result4").style.display = "none";
+        }
+        break;
+        case 2: {
+            document.querySelector("#result3").style.display = "block";
+            document.getElementById("result3").innerHTML = "Fique em casa e cuide da sua saúde!";
+            document.querySelector("#result1").style.display = "none";
+            document.querySelector("#result2").style.display = "none";
+            document.querySelector("#result4").style.display = "none";
+        }
+        break;
+        case 3: {
+            document.querySelector("#result4").style.display = "block";
+            document.getElementById("result4").innerHTML = "Faça compras pelos mais idosos e use máscara!";
+            document.querySelector("#result1").style.display = "none";
+            document.querySelector("#result2").style.display = "none";
+            document.querySelector("#result3").style.display = "none";
+        }
+        break;
+        default: console.log("Algo de errado não está certo");
+}
+}
+
+
+function saudavelJs(){
+    let saudavelf4 = 3;
+    switch (saudavelf4){
+    case 0: {
+        document.querySelector("#result2").style.display = "block";
+        document.getElementById("result2").innerHTML = "Fique em casa, peça para fazerem compras por você!";
+        document.querySelector("#result2").style.display = "none";
+        document.querySelector("#result3").style.display = "none";
+        document.querySelector("#result4").style.display = "none";
+        }
+        break;
+        case 1: {
+            document.querySelector("#result2").style.display = "block";
+            document.getElementById("result2").innerHTML = "Fique em casa, tome Sol e faça exercícios leves!";
+            document.querySelector("#result1").style.display = "none";
+            document.querySelector("#result3").style.display = "none";
+            document.querySelector("#result4").style.display = "none";
+        }
+        break;
+        case 2: {
+            document.querySelector("#result3").style.display = "block";
+            document.getElementById("result3").innerHTML = "Fique em casa e cuide da sua saúde!";
+            document.querySelector("#result1").style.display = "none";
+            document.querySelector("#result2").style.display = "none";
+            document.querySelector("#result4").style.display = "none";
+        }
+        break;
+        case 3: {
+            document.querySelector("#result4").style.display = "block";
+            document.getElementById("result4").innerHTML = "Faça compras pelos mais idosos e use máscara!";
+            document.querySelector("#result1").style.display = "none";
+            document.querySelector("#result2").style.display = "none";
+            document.querySelector("#result3").style.display = "none";
+        }
+        break;
+        default: console.log("Algo de errado não está certo");
+}
+   
+}
